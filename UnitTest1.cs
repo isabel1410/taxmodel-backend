676bf3d27f32db1fd642d7a77e93051fc76f62fe
isabel1410 <97336074+isabel1410@users.noreply.github.com>
2022-01-08 16:06:15 +0100

    tests

---
+++ UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Taxmodel_backend_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Dit is een random comment voor een test
+            Assert.IsTrue(true);
+        }
+    }
+}
